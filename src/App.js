216c2e0b75eff3abd96611379ee148ec74bddd61
nxf87957 <athulya.arokiyasamy@nxp.com>
2022-12-20 12:23:15 +0530

    QR code display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import qr from './images/image-qr-code.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='qr-container'>
+      <div className='qr-item'>
+        <img className='qr-image' alt="qr" src={qr} />
+        <div>
+          <h4>Improve your front-end skills by building projects</h4>
+          <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </div>
     </div>
   );
 }
